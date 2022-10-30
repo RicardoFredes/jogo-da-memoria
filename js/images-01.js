1db16e856c571660f7ba0a02c0b7fc46dc04a549
fredes <ricardofredes@gmail.com>
2022-10-30 15:40:44 -0300

    feat: add images

--- js/images-01.js
+++ js/images-01.js
@@ -1,8 +1,8 @@
 const images01 = [
-  "/jogo-da-memoria/img/cards/donald.webp",
+  "/jogo-da-memoria/img/cards/jose-01.jpg",
   "/jogo-da-memoria/img/cards/margarida.jpg",
   "/jogo-da-memoria/img/cards/mickey.jpg",
   "/jogo-da-memoria/img/cards/minnie.webp",
-  "/jogo-da-memoria/img/cards/pateta.jpg",
+  "/jogo-da-memoria/img/cards/jose-comilao.webp",
   "/jogo-da-memoria/img/cards/pluto.jpg",
 ]
