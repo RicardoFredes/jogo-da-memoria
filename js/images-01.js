7eef692706a70d4a0288b919838f73e6850a95a4
fredes <ricardofredes@gmail.com>
2022-10-30 15:53:44 -0300

    feat: add images

--- js/images-01.js
+++ js/images-01.js
@@ -1,6 +1,6 @@
 const images01 = [
   "/jogo-da-memoria/img/cards/jose-01.jpg",
-  "/jogo-da-memoria/img/cards/margarida.jpg",
+  "/jogo-da-memoria/img/cards/cuquin.jpg",
   "/jogo-da-memoria/img/cards/mickey.jpg",
   "/jogo-da-memoria/img/cards/minnie.webp",
   "/jogo-da-memoria/img/cards/jose-comilao.webp",
